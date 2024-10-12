1fe90ba10b1d77a4d58d7b38ff6e36f1fb22edb1
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-10-12 13:21:04 -0300

    Can create crate for specific day

--- create_new_day/src/main.rs
+++ create_new_day/src/main.rs
@@ -1,3 +1,22 @@
+use std::process::Command;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    day_number: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let crate_name = format!("day_{:02}", args.day_number);
+    println!("Creating crate {crate_name}...");
+
+    Command::new("cargo")
+        .args(["workspaces", "create"])
+        .args(["--bin", &crate_name])
+        .args(["--name", &crate_name])
+        .args(["--edition", "2021"])
+        .output()
+        .expect("Failed to create cargo library");
 }
