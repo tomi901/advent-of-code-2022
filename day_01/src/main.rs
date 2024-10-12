971eaa08544c379af5dda953641648d5ecbc4e43
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-10-12 14:15:07 -0300

    Can initialize day crates with correct template file

---
+++ day_01/src/main.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    let input = std::fs::read_to_string("./input.txt").expect("Error reading input file.");
+    for line in input.lines() {
+        // Process lines
+    }
+
+    println!("Result:");
+}
