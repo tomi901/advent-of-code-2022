83ee1c196c5ffc3a9e4aa0503eeed17a5af4e967
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-10-12 14:29:18 -0300

    Day 1 - Part 1 and 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,9 +1,26 @@
+use std::cmp::Reverse;
+
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt").expect("Error reading input file.");
-    for line in input.lines() {
-        // Process lines
-    }
 
+    // If we had yield to easily create iterators, this would've been so much easier
+    let mut total_calories = 0;
+    let mut calories_sums = Vec::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            calories_sums.push(total_calories);
+            total_calories = 0;
+            continue;
+        }
+
+        let calories: i64 = line.parse().unwrap();
+        total_calories += calories;
+    }
+    calories_sums.push(total_calories);
+    calories_sums.sort_by_key(|&c| Reverse(c));
+
+    let result: i64 = calories_sums.iter().take(3).sum();
     println!("Result:");
+    println!("{result}");
 }
