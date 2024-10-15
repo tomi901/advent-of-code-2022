00dc06b41c7c8eba5d7ef1e318e3c29a44179dee
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-10-15 11:58:01 -0300

    Day 6 - Part 1

---
+++ day_06/src/main.rs
@@ -0,0 +1,48 @@
+use std::{collections::HashSet, fmt::Display};
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
+
+fn main() {
+    part_1();
+    // part_2();
+}
+
+fn part_1() {
+    let input = std::fs::read_to_string("./input.txt").expect("Error reading input file.");
+    
+    let result: usize = input.lines()
+        .flat_map(find_signal_start_index)
+        .sum();
+
+    display_result(&result);
+}
+
+fn part_2() {
+
+}
+
+fn find_signal_start_index(s: &str) -> Option<usize> {
+    (4..s.len())
+        .map(|i| (i, &s[(i - 4)..i]))
+        .filter(|&(_, bytes)| {
+            let mut lookup = HashSet::new();
+            for b in bytes.bytes() {
+                if lookup.contains(&b) {
+                    return false;
+                }
+                lookup.insert(b);
+            }
+            return true;
+        })
+        .find_map(|(i, _)| Some(i))
+}
+
+// TODO: Move this to common library crate
+fn display_result<T: Display>(result: &T) {
+    println!("Result:");
+    let str_result = format!("{}", result);
+    println!("{}", &str_result);
+
+    let mut clipboard = ClipboardContext::new().unwrap();
+    clipboard.set_contents(str_result.clone()).unwrap();
+    println!("Copied result to clipboard!");
+}
