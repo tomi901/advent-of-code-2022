ada4690c82e6a9ac61d373cb89efcfdfc30a7b25
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-10-17 15:11:37 -0300

    Added common library for maps

---
+++ xmas/src/direction.rs
@@ -0,0 +1,30 @@
+use crate::point2d::Point2D;
+use Direction::*;
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+
+pub const DIRECTIONS: [Direction; 4] = [North, East, South, West];
+
+impl Direction {
+    pub fn as_point(&self) -> Point2D {
+        match self {
+            North => Point2D(0, -1),
+            East => Point2D(1, 0),
+            South => Point2D(0, 1),
+            West => Point2D(-1, 0),
+        }
+    }
+}
+
+impl From<Direction> for Point2D {
+    fn from(value: Direction) -> Self {
+        value.as_point()
+    }
+}
---
+++ xmas/src/point2d.rs
@@ -0,0 +1,51 @@
+use std::{fmt::Display, ops};
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default, Hash)]
+pub struct Point2D(pub isize, pub isize);
+
+impl Display for Point2D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+impl ops::Add<Point2D> for Point2D {
+    type Output = Point2D;
+
+    fn add(self, rhs: Point2D) -> Self::Output {
+        Point2D(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl ops::AddAssign<Point2D> for Point2D {
+    fn add_assign(&mut self, rhs: Point2D) {
+        *self = *self + rhs
+    }
+}
+
+impl ops::Sub<Point2D> for Point2D {
+    type Output = Point2D;
+
+    fn sub(self, rhs: Point2D) -> Self::Output {
+        Point2D(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl ops::SubAssign<Point2D> for Point2D {
+    fn sub_assign(&mut self, rhs: Point2D) {
+        *self = *self - rhs
+    }
+}
+
+impl From<(isize, isize)> for Point2D {
+    fn from(value: (isize, isize)) -> Self {
+        Self(value.0, value.1)
+    }
+}
+
+impl From<[isize; 2]> for Point2D {
+    fn from(value: [isize; 2]) -> Self {
+        Self(value[0], value[1])
+    }
+}
