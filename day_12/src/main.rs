029b2e60b4b2da6c03bd9149d812d8ab55d7c5a1
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-10-21 12:34:42 -0300

    Day 12 - Part 2

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -37,14 +37,23 @@ impl NavigationMap {
     }
 
     pub fn find_path(&self) -> Option<(Vec<Point2D>, usize)> {
+        self.find_path_from(self.start)
+    }
+
+    pub fn find_path_from(&self, start: Point2D) -> Option<(Vec<Point2D>, usize)> {
         const MOVE_COST: usize = 1;
         let destination = self.destination;
         astar(
-            &self.start,
+            &start,
             |&from| self.find_navigatable_tiles(from).map(|p| (p, MOVE_COST)),
             |cur| cur.manhattan_distance(destination),
             |&cur| cur == destination,
         )
+    }
+
+    pub fn lowest_points(&self) -> impl Iterator<Item = Point2D> + '_ {
+        self.map.iter_points()
+            .filter(|&p| self.map.get_tile(p).is_some_and(|&t| t == b'a'))
     }
 
     fn find_navigatable_tiles(&self, from: Point2D) -> impl Iterator<Item = Point2D> + '_ {
@@ -67,12 +76,22 @@ fn part_1() {
     let input = std::fs::read_to_string("./input.txt").expect("Error reading input file.");
     let map = NavigationMap::from_str(&input).unwrap();
 
-    let (path, _) = map.find_path().unwrap();
-    let result = path.len() - 1;
+    let (_, cost) = map.find_path().unwrap();
+    println!("Shortest cost: {cost}");
 
-    display_result(&result);
+    display_result(&cost);
 }
 
 fn part_2() {
-    
+    let input = std::fs::read_to_string("./input.txt").expect("Error reading input file.");
+    let map = NavigationMap::from_str(&input).unwrap();
+
+    let (start, _, cost) = map.lowest_points()
+        .flat_map(|start| map.find_path_from(start).map(|(path, cost)| (start, path, cost)))
+        .min_by_key(|(_, path, _)| path.len())
+        .unwrap();
+
+    println!("Shortest found at: {start}, cost: {cost}");
+
+    display_result(&cost);
 }
