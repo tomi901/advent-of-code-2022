183079daa5d033ab42256a071c21d6671318d242
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-11-07 13:45:44 -0300

    Optimized version with pure nodes

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Reverse, collections::{BinaryHeap, HashMap, HashSet}, ops::Not, rc::Rc, str::FromStr};
+use std::{collections::{BinaryHeap, HashMap, HashSet}, rc::Rc, str::FromStr};
 
 use anyhow::{self, Context};
 use regex_static::{lazy_regex, Regex, once_cell::sync::Lazy};
@@ -9,10 +9,12 @@ static VALVE_REGEX: Lazy<Regex> = lazy_regex!(r"Valve (\S+) .*rate=(\d+).*valves
 type ValveId = String;
 const START_ID: &str = "AA";
 
+#[derive(Debug, Clone)]
 struct Valve {
     id: ValveId,
     rate: usize,
     leads_to: Vec<ValveId>,
+    path_costs: HashMap<ValveId, usize>,
 }
 
 impl FromStr for Valve {
@@ -28,220 +30,272 @@ impl FromStr for Valve {
             .split(',')
             .map(|s| s.trim().to_string())
             .collect::<Vec<_>>();
-        Ok(Valve { id, rate, leads_to })
+        Ok(Valve { id, rate, leads_to, path_costs: Default::default() })
     }
 }
 
+#[derive(Debug, Clone)]
 struct ValveSystem {
     valves: HashMap<ValveId, Valve>,
-    ordered_valve_rates: Vec<(ValveId, usize)>,
 }
 
 impl ValveSystem {
+    pub fn try_new(valves: impl Iterator<Item = Valve>) -> Result<Self, anyhow::Error> {
+        let valves_map: HashMap<String, Valve> = valves
+            .map(|v| (v.id.clone(), v))
+            .collect();
+        if !valves_map.contains_key(START_ID) {
+            Err(anyhow::anyhow!("No valve with id {} found", START_ID))?;
+        }
+
+        let mut system = ValveSystem { valves: valves_map };
+        system.recalculate_paths();
+        // println!("Created valve system:");
+        // println!("{:#?}", system);
+        Ok(system)
+    }
+
+    fn recalculate_paths(&mut self) {
+        let mut costs = Vec::new();
+        let valid_targets = self.valves
+            .values()
+            .filter(|v| v.rate > 0)
+            .map(|v| v.id.as_str())
+            .collect::<Vec<_>>();
+        for from in self.valves.values() {
+            let path_costs: HashMap<_, _> = valid_targets
+                .iter()
+                .map(|to| self.calculate_path_cost(&from.id, to).map(|cost| (to.to_string(), cost)).unwrap())
+                .collect();
+            costs.push((from.id.clone(), path_costs));
+        }
+
+        for (from, path_costs) in costs {
+            let valve = self.valves.get_mut(&from).unwrap();
+            valve.path_costs = path_costs;
+        }
+    }
+
+    fn calculate_path_cost(&self, from: &str, to: &str) -> Option<usize> {
+        // println!("Calculating path {from} -> {to}");
+
+        let mut closed_list = HashSet::new();
+        // This probably could've been a Queue
+        let mut open_list = BinaryHeap::<PathBreadcrumbPriority>::new();
+        open_list.push(PathBreadcrumbPriority(PathBreadcrumb { from: "", to: from, cost: 0 }));
+        
+        while !open_list.is_empty() {
+            let candidate = open_list.pop().unwrap().0;
+            if candidate.to == to {
+                return Some(candidate.cost);
+            }
+
+            if closed_list.contains(&(candidate.from, candidate.to)) {
+                continue;
+            }
+            closed_list.insert((candidate.from, candidate.to));
+
+            let valve = self.valves.get(candidate.to).unwrap();
+            let next_candidates = valve.leads_to.iter()
+                .filter(|s| !closed_list.contains(&(candidate.to, s.as_str())))
+                .map(|s| PathBreadcrumb {
+                    from: candidate.to,
+                    to: s.as_str(),
+                    cost: candidate.cost + 1,
+                })
+                .map(PathBreadcrumbPriority);
+            open_list.extend(next_candidates);
+
+            // println!("{} candidates in queue...", open_list.len())
+        }
+        None
+    }
+
     pub fn calculate_greatest_amount_of_pressure(&self, time_limit: usize) -> usize {
         let mut max_pressure = 0;
 
-        let mut open_list = BinaryHeap::<BreadcrumbPriority>::new();
+        let start = self.valves.get(START_ID).unwrap();
+
         let mut closed_list = HashSet::new();
-        open_list.push(Breadcrumb::start(time_limit).into());
+        let mut open_list = BinaryHeap::<ValveBreadcrumbPriority>::new();
+        open_list.push(ValveBreadcrumb {
+            from: "",
+            to: &start.id,
+            final_pressure: 0,
+            time_left: time_limit,
+            previous: None,
+        }.into());
+
         while !open_list.is_empty() {
             let candidate = Rc::new(open_list.pop().unwrap().0);
-            closed_list.insert(candidate.as_ref().clone());
-
-            if candidate.optimistic_final_pressure <= max_pressure {
+            if closed_list.contains(&candidate) {
                 continue;
             }
+            closed_list.insert(candidate.clone());
 
-            // println!("Checking {} at minute {}...", candidate.valve, candidate.time);
             if candidate.final_pressure > max_pressure {
                 max_pressure = candidate.final_pressure;
                 // With the real input it won't finish but after some minutes the answer is shown here:
                 println!("Found candidate pressure: {}", max_pressure);
+
+                // let mut path = vec![];
+                // let mut cur = &candidate;
+                // loop {
+                //     path.push(cur.clone());
+
+                //     match &cur.previous {
+                //         Some(previous) => cur = previous,
+                //         None => break,
+                //     }
+                // }
+                // path.reverse();
+
+                // println!("Items: {}", path.len());
+                // print!("[");
+                // let mut first = true;
+                // for bc in path {
+                //     if !first {
+                //         print!(", ");
+                //     }
+                //     first = false;
+                //     print!("{} ({})", bc.to, bc.time_left);
+                // }
+                // println!("]");
             }
 
-            if candidate.time_left > 0 {
-                open_list.extend(
-                    self.find_candidates(candidate)
-                        .map(|b| self.recalculate_optimistic_final_pressure(b))
-                        .filter(|b| !closed_list.contains(b))
-                        .map(BreadcrumbPriority),
-                );
-            }
+            open_list.extend(
+                self.find_candidates(candidate)
+                    .filter(|b| !closed_list.contains(b))
+                    .map(ValveBreadcrumbPriority),
+            );
         }
 
         max_pressure
     }
 
-    fn recalculate_optimistic_final_pressure(&self, breadcrumb: Breadcrumb) -> Breadcrumb {
-        let mut time_left = breadcrumb.time_left;
-        if time_left <= 1 {
-            return Breadcrumb {
-                optimistic_final_pressure: breadcrumb.final_pressure,
-                ..breadcrumb
-            };
-        }
+    fn find_candidates<'a>(&'a self, cur: Rc<ValveBreadcrumb<'a>>) -> impl Iterator<Item = ValveBreadcrumb<'a>> + '_ {
+        let from = cur.to;
+        let valve = self.valves.get(from).unwrap();
+        let time_left = cur.time_left;
+        let final_pressure = cur.final_pressure;
 
-        let opened_valves = breadcrumb.trace_open_valves().collect::<HashSet<_>>();
-        let try_valves = self.ordered_valve_rates
+        valve.path_costs
             .iter()
-            .filter(|(v, _)| !opened_valves.contains(v));
-        time_left -= 1;
-
-        let mut optimistic_bonus = 0;
-        for (_, rate) in try_valves {
-            optimistic_bonus += time_left * rate;
-            if time_left < 3 {
-                break;
-            }
-            time_left -= 2;
-        }
-        // println!("optimistic_bonus: {optimistic_bonus}");
-
-        Breadcrumb {
-            optimistic_final_pressure: breadcrumb.final_pressure + optimistic_bonus,
-            ..breadcrumb
-        }
-    }
-
-    fn find_candidates(&self, cur: Rc<Breadcrumb>) -> impl Iterator<Item = Breadcrumb> + '_ {
-        let valve = self.valves.get(&cur.valve).unwrap();
-        let new_time_left = cur.time_left - 1;
-        let open_action = cur.has_opened_cur()
-            .not()
-            .then(|| Breadcrumb {
-                valve: cur.valve.clone(),
-                time_left: new_time_left,
-                action: Action::Open,
-                final_pressure: cur.final_pressure + (new_time_left * valve.rate),
-                optimistic_final_pressure: 0,
-                previous: Some(cur.clone()),
-            });
-
-        let move_actions = valve.leads_to
-            .iter()
-            .map(move |id| Breadcrumb {
-                valve: id.clone(),
-                time_left: new_time_left,
-                action: Action::Move,
-                final_pressure: cur.final_pressure,
-                optimistic_final_pressure: 0,
-                previous: Some(cur.clone()),
-            });
-
-        open_action.into_iter().chain(move_actions)
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Breadcrumb {
-    valve: ValveId,
-    time_left: usize,
-    action: Action,
-    final_pressure: usize,
-    optimistic_final_pressure: usize,
-    previous: Option<Rc<Self>>,
-}
-
-impl Breadcrumb {
-    fn start(time_left: usize) -> Self {
-        Self {
-            valve: START_ID.to_string(),
-            time_left,
-            action: Action::Move,
-            final_pressure: 0,
-            optimistic_final_pressure: usize::MAX,
-            previous: None,
-        }
-    }
-
-    fn trace_open_valves(&self) -> impl Iterator<Item = &ValveId> + '_ {
-        let mut cur = self;
-        let mut done = false;
-        std::iter::from_fn(move || {
-            if done {
-                return None;
-            }
-            loop {
-                let id: Option<&String> = (cur.action == Action::Open).then_some(&cur.valve);
-
-                match &cur.previous {
-                    Some(previous) => cur = previous.as_ref(),
-                    None => {
-                        done = true;
-                        return id;
-                    },
+            .filter(move |(_, cost)| (*cost + 1) < time_left)
+            .map(move |(id, cost)| {
+                // println!("{} -> {} will cost {} for time left {}", from, id, cost, time_left);
+                (self.valves.get(id).unwrap(), cost + 1, cur.clone())
+            })
+            .filter(|(id, _, previous)| !previous.has_visited(&id.id))
+            .map(move |(target_valve, cost, previous)| {
+                let new_time_left = time_left - cost;
+                let add_pressure = target_valve.rate * new_time_left;
+                ValveBreadcrumb {
+                    from,
+                    to: &target_valve.id,
+                    time_left: new_time_left,
+                    final_pressure: final_pressure + add_pressure,
+                    previous: Some(previous),
                 }
-
-                if id.is_some() {
-                    return id;
-                }
-            }
-        })
+            })
     }
-
-    fn has_opened(&self, id: &str) -> bool {
-        self.trace_open_valves().any(|v| v == id)
-    }
-
-    fn has_opened_cur(&self) -> bool {
-        self.has_opened(&self.valve)
-    }
-}
-
-#[derive(Debug, Clone)]
-struct BreadcrumbPriority(Breadcrumb);
-
-impl From<Breadcrumb> for BreadcrumbPriority {
-    fn from(value: Breadcrumb) -> Self {
-        BreadcrumbPriority(value)
-    }
-}
-
-impl PartialEq for BreadcrumbPriority {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.final_pressure == other.0.final_pressure
-    }
-}
-
-impl Eq for BreadcrumbPriority {
-}
-
-impl PartialOrd for BreadcrumbPriority {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for BreadcrumbPriority {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.0.final_pressure.cmp(&other.0.final_pressure)
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-enum Action {
-    Move,
-    Open,
 }
 
 impl FromStr for ValveSystem {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let valves: HashMap<String, Valve> = s.lines()
+        let valves: Vec<Valve> = s.lines()
             .map(Valve::from_str)
-            .map(|v| v.map(|v| (v.id.clone(), v)))
             .collect::<Result<_, _>>()?;
-        if !valves.contains_key(START_ID) {
-            Err(anyhow::anyhow!("No valve with id {} found", START_ID))?;
+        
+        Self::try_new(valves.into_iter())
+    }
+}
+
+#[derive(Debug, Clone)]
+struct PathBreadcrumb<'a> {
+    from: &'a str,
+    to: &'a str,
+    cost: usize,
+}
+
+#[derive(Debug, Clone)]
+struct PathBreadcrumbPriority<'a>(PathBreadcrumb<'a>);
+
+impl<'a> PartialEq for PathBreadcrumbPriority<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.cost == other.0.cost
+    }
+}
+
+impl<'a> Eq for PathBreadcrumbPriority<'a> {
+}
+
+impl<'a> PartialOrd for PathBreadcrumbPriority<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for PathBreadcrumbPriority<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cost.cmp(&other.0.cost).reverse()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct ValveBreadcrumb<'a> {
+    from: &'a str,
+    to: &'a str,
+    time_left: usize,
+    final_pressure: usize,
+    previous: Option<Rc<Self>>,
+}
+
+impl<'a> ValveBreadcrumb<'a> {
+    pub fn has_visited(&self, id: &str) -> bool {
+        let mut cur = self;
+        loop {
+            if cur.to == id {
+                return true;
+            }
+
+            match &cur.previous {
+                Some(previous) => cur = previous.as_ref(),
+                None => return false,
+            }
         }
+    }
+}
 
-        let mut ordered_valve_rates = valves.values()
-            .map(|v| (v.id.clone(), v.rate))
-            .collect::<Vec<_>>();
-        ordered_valve_rates.sort_by_key(|v| Reverse(v.1));
+#[derive(Debug, Clone)]
+struct ValveBreadcrumbPriority<'a>(ValveBreadcrumb<'a>);
 
-        Ok(ValveSystem { valves, ordered_valve_rates })
+impl<'a> From<ValveBreadcrumb<'a>> for ValveBreadcrumbPriority<'a> {
+    fn from(value: ValveBreadcrumb<'a>) -> Self {
+        Self(value)
+    }
+}
+
+impl<'a> PartialEq for ValveBreadcrumbPriority<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.final_pressure == other.0.final_pressure
+    }
+}
+
+impl<'a> Eq for ValveBreadcrumbPriority<'a> {
+}
+
+impl<'a> PartialOrd for ValveBreadcrumbPriority<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for ValveBreadcrumbPriority<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.final_pressure.cmp(&other.0.final_pressure)
     }
 }
 
