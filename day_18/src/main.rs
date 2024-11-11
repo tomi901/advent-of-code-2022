227dbb3c4b05d922402f7728b2344674342cb283
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-11-11 10:58:59 -0300

    Day 18 - Part 1

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,5 +1,7 @@
+use std::{collections::HashSet, str::FromStr};
+
 use anyhow::{self, Context};
-use xmas::display_result;
+use xmas::{direction3d::DIRECTIONS_3D, display_result, point3d::Point3D};
 
 fn main() -> anyhow::Result<()> {
     part_1()?;
@@ -12,11 +14,10 @@ fn part_1() -> anyhow::Result<()> {
     println!("Part 1:");
     let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
-    for line in input.lines() {
-        // Process lines
-    }
+    let points: Vec<_> = input.lines().map(Point3D::from_str).collect::<Result<_, _>>()?;
+    let result = calculate_surface_area(&points);
 
-    // display_result(&result);
+    display_result(&result);
     Ok(())
 }
 
@@ -24,4 +25,18 @@ fn part_2() -> anyhow::Result<()> {
     println!("Part 2:");
 
     Ok(())
+}
+
+fn calculate_surface_area(points: &[Point3D]) -> usize {
+    let mut already_placed = HashSet::new();
+    let mut area = 0;
+    for &point in points.iter() {
+        area += DIRECTIONS_3D.len();
+        area -= DIRECTIONS_3D.iter()
+            .map(|d| point + d.as_point())
+            .filter(|p| already_placed.contains(p))
+            .count() * 2;
+        already_placed.insert(point);
+    }
+    area
 }
