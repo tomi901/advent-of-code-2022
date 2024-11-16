ff24c6680d05470637c13e20d50b433158a58ab1
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-11-16 10:35:46 -0300

    Day 21 - Part 2

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -2,6 +2,10 @@ use std::{collections::HashMap, str::FromStr};
 
 use anyhow::{self, Context};
 use xmas::display_result;
+use self::MonkeyResult::*;
+
+const ROOT: &str = "root";
+const HUMAN: &str = "humn";
 
 fn main() -> anyhow::Result<()> {
     part_1()?;
@@ -15,7 +19,7 @@ fn part_1() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
     let monkeys = MonkeyGroup::from_str(&input)?;
-    let result = monkeys.eval("root")?;
+    let result = monkeys.eval(ROOT)?;
 
     display_result(&result);
     Ok(())
@@ -23,10 +27,16 @@ fn part_1() -> anyhow::Result<()> {
 
 fn part_2() -> anyhow::Result<()> {
     println!("Part 2:");
+    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
+    let monkeys = MonkeyGroup::from_str(&input)?;
+    let result = monkeys.solve_human_value()?;
+
+    display_result(&result);
     Ok(())
 }
 
+#[derive(Debug, Clone)]
 enum MonkeyValue {
     Number(i64),
     Operation(String, Operation, String),
@@ -56,11 +66,38 @@ impl MonkeyValue {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 enum Operation {
     Add,
     Sub,
     Mul,
     Div,
+}
+
+impl Operation {
+    pub fn apply(&self, lhs: i64, rhs: i64) -> i64 {
+        match self {
+            Operation::Add => lhs + rhs,
+            Operation::Sub => lhs - rhs,
+            Operation::Mul => lhs * rhs,
+            Operation::Div => lhs / rhs,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum MonkeyResult<'a> {
+    Known(i64),
+    Unknown(&'a str),
+}
+
+impl<'a> MonkeyResult<'a> {
+    pub fn from_result(id: &'a str, result: Option<i64>) -> Self {
+        match result {
+            Some(n) => Known(n),
+            None => Unknown(id),
+        }
+    }
 }
 
 struct MonkeyGroup {
@@ -78,14 +115,95 @@ impl MonkeyGroup {
             MonkeyValue::Operation(lhs, op, rhs) => {
                 let left = self.eval(lhs)?;
                 let right = self.eval(rhs)?;
-                match op {
-                    Operation::Add => left + right,
-                    Operation::Sub => left - right,
-                    Operation::Mul => left * right,
-                    Operation::Div => left / right,
-                }
+                op.apply(left, right)
             },
         })
+    }
+
+    pub fn eval_non_human(&self, monkey_id: &str) -> Result<Option<i64>, anyhow::Error> {
+        if monkey_id == HUMAN {
+            return Ok(None);
+        }
+
+        let monkey = self.monkeys
+            .get(monkey_id)
+            .with_context(|| format!("No monkey with id '{monkey_id}' found"))?;
+
+        Ok(match monkey {
+            MonkeyValue::Number(n) => Some(*n),
+            MonkeyValue::Operation(lhs, op, rhs) => {
+                let left = match self.eval_non_human(lhs)? {
+                    Some(n) => n,
+                    None => return Ok(None),
+                };
+                let right = match self.eval_non_human(rhs)? {
+                    Some(n) => n,
+                    None => return Ok(None),
+                };
+                Some(op.apply(left, right))
+            },
+        })
+    }
+
+    pub fn solve_human_value(&self) -> Result<i64, anyhow::Error> {
+        let root = self.monkeys
+            .get(ROOT)
+            .with_context(|| format!("No root monkey found"))?;
+        let results = match root {
+            MonkeyValue::Operation(lhs, _, rhs) =>
+                (
+                    MonkeyResult::from_result(lhs, self.eval_non_human(lhs)?),
+                    MonkeyResult::from_result(rhs, self.eval_non_human(rhs)?),
+                ),
+            _ => unreachable!(),
+        };
+
+        match results {
+            (Known(expected), Unknown(x)) =>
+                self.solve_for_result(x, expected),
+            (Unknown(x), Known(expected)) =>
+                self.solve_for_result(x, expected),
+            _ => unreachable!(),
+        }
+    }
+
+    fn solve_for_result(&self, monkey_id: &str, expected: i64) -> Result<i64, anyhow::Error> {
+        if monkey_id == HUMAN {
+            return Ok(expected);
+        }
+
+        let monkey = self.monkeys
+            .get(monkey_id)
+            .with_context(|| format!("No root monkey found"))?;
+        let operation = match monkey {
+            MonkeyValue::Operation(lhs, op, rhs) =>
+                (
+                    MonkeyResult::from_result(lhs, self.eval_non_human(lhs)?),
+                    op,
+                    MonkeyResult::from_result(rhs, self.eval_non_human(rhs)?),
+                ),
+            _ => unreachable!("Unreacheable for {monkey_id}: {:?}", monkey),
+        };
+        
+        let (x, x_expected) = match operation {
+            (Known(n), Operation::Add, Unknown(x)) |
+            (Unknown(x), Operation::Add, Known(n)) =>
+                (x, expected - n),
+            (Known(n), Operation::Mul, Unknown(x)) |
+            (Unknown(x), Operation::Mul, Known(n)) =>
+                (x, expected / n),
+            // n - x = expected => x = n - expected
+            (Known(n), Operation::Sub, Unknown(x)) => (x, n - expected),
+            // x - n = expected => x = expected + n
+            (Unknown(x), Operation::Sub, Known(n)) => (x, expected + n),
+            // n / x = expected => n = expected * x => x = n / expected
+            (Known(n), Operation::Div, Unknown(x)) => (x, n / expected),
+            // x / n = expected => x = expected * n
+            (Unknown(x), Operation::Div, Known(n)) => (x, expected * n),
+            _ => unreachable!("Unreacheable pattern: {:?}", operation),
+        };
+
+        self.solve_for_result(x, x_expected)
     }
 }
 
